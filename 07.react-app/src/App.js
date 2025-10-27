3a973bb8f0abbc10d3e8933e2d6b8d796767b083
kareem <kshaik_sr2021@yahoo.com>
2025-10-27 19:52:50 +0000

    React Create Element Example

--- 07.react-app/src/App.js
+++ 07.react-app/src/App.js
@@ -1,3 +1,4 @@
+import React, {createElement} from 'react';
 import './App.css';
 
 function App() {
@@ -9,6 +10,21 @@ function App() {
   //   </div>
   // );
 */
+// return React.createElement('div', null, "**React with Redux Training**");
+// return React.createElement('div', null, "<h2>**React with Redux Training**</h2>");
+// return React.createElement('div', null, "<h2>", "**React with Redux Training**");
+// return React.createElement('div', null, <h2>"**React with Redux Training**"</h2>);
+// return React.createElement('div', {id: "header", className: "headClass"}, React.createElement('h2', {}, "**React with Redux Training**"));
+// return React.createElement('div', {id: 'header', className: 'headClass'}, React.createElement('h2', {style: {color: 'blue', textShadow: '0 0 10px black'}}, "**React with Redux Training**"));
+// return React.createElement('div', {style: {width: '600px', textAlign: 'center', borderRadius: '10px', boxShadow: '0 0 10px black', padding: '10px', margin: 'auto'}},
+//           React.createElement('h2', {}, 'Shaik Solutions Private Limited'),
+//           React.createElement('img', {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy9MV_G-_OO6HiCJisrMctVAyTpbnnkD3vEg&s', width: '200px', alt: 'Google for VizaG', style:{borderRadius: '25px'}}),
+//           React.createElement('h3', {}, 'React with Redux Training'));
+
+return createElement('div', {style: {width: '600px', textAlign: 'center', borderRadius: '10px', boxShadow: '0 0 10px black', padding: '10px', margin: 'auto'}},
+          createElement('h2', {}, 'Shaik Solutions Private Limited'),
+          createElement('img', {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy9MV_G-_OO6HiCJisrMctVAyTpbnnkD3vEg&s', width: '200px', alt: 'Google for VizaG', style:{borderRadius: '25px'}}),
+          createElement('h3', {}, 'React with Redux Training'));          
 }
 
 export default App;
